8be5704f98faebcec5ff978ce44c99cd4840feab
Dave Kiss <dave@davekiss.com>
2017-05-23 21:46:54 -0600

    Update header with homelink and basic nav

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,10 +1,8 @@
-<header class="banner">
-  <div class="container">
-    <a class="brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
-    <nav class="nav-primary">
-      @if (has_nav_menu('primary_navigation'))
-        {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
-      @endif
-    </nav>
-  </div>
+<header class="fixed w-100 ph3 pv3 pv4-ns ph4-m ph5-l">
+	<a class="f3 brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
+	<nav class="f6 fw6 ttu tracked nav-primary">
+		@if (has_nav_menu('primary_navigation'))
+			{!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
+		@endif
+	</nav>
 </header>
