344c802b60cf5ef28c71c64fec6bbc85751a14e2
Dave Kiss <dave@davekiss.com>
2017-05-23 21:45:36 -0600

    Add Khula typeface

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -2,5 +2,6 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link href="https://fonts.googleapis.com/css?family=Khula:300,400,600,700" rel="stylesheet">
   @php(wp_head())
 </head>
