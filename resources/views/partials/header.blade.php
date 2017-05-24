23236f232a6d33eec3e693defdd1553c4a210c10
Dave Kiss <dave@davekiss.com>
2017-05-23 22:38:07 -0600

    Add nav class to header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,4 +1,4 @@
-<header class="fixed w-100 ph3 pv3 pv4-ns ph4-m ph5-l">
+<header class="nav fixed w-100 ph3 pv3 pv4-ns ph4-m ph5-l">
 	<a class="f3 brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
 	<nav class="f6 fw6 ttu tracked nav-primary">
 		@if (has_nav_menu('primary_navigation'))
