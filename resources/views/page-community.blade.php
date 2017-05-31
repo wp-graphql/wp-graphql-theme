21d65c898be966265849cc790d50d0013da15b26
Dave Kiss <dave@davekiss.com>
2017-05-31 15:27:49 -0400

    Add a community page

---
+++ resources/views/page-community.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.uncontained')
+
+@section('content')
+	<article class="hero center ph3 ph5-ns tc pv7 near-black">
+		<h1 class="b f1 f2-m lh-title fw7 tc ma0">Join the Slack Channel</h1>
+		<p class="f4 measure lh-copy center">The team working on WPGraphQL uses Slack to communicate ideas and foster community. If you'd like to participate, you can join the Slack channel through the link below.</p>
+		<a target="_blank" class="no-underline near-white grow inline-flex items-center ma2 tc br2 pa2" href="https://slackin-wpgraphql.herokuapp.com/" title="Slack">
+			<svg width="40px" height="40px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 270 270">
+			  <style>
+			    .st0{fill:#ECB32D;} .st1{fill:#63C1A0;} .st2{fill:#E01A59;} .st3{fill:#331433;} .st4{fill:#D62027;} .st5{fill:#89D3DF;} .st6{fill:#258B74;} .st7{fill:#819C3C;}
+			  </style>
+			  <path class="st0" d="M154.3 87.1c-1.9-5.7-8-8.8-13.7-7-5.7 1.9-8.8 8-7 13.7l28.1 86.4c1.9 5.3 7.7 8.3 13.2 6.7 5.8-1.7 9.3-7.8 7.4-13.4 0-.2-28-86.4-28-86.4z"/>
+			  <path class="st1" d="M110.8 101.2c-1.9-5.7-8-8.8-13.7-7-5.7 1.9-8.8 8-7 13.7l28.1 86.4c1.9 5.3 7.7 8.3 13.2 6.7 5.8-1.7 9.3-7.8 7.4-13.4 0-.2-28-86.4-28-86.4z"/>
+			  <path class="st2" d="M189.7 158.6c5.7-1.9 8.8-8 7-13.7-1.9-5.7-8-8.8-13.7-7l-86.5 28.2c-5.3 1.9-8.3 7.7-6.7 13.2 1.7 5.8 7.8 9.3 13.4 7.4.2 0 86.5-28.1 86.5-28.1z"/>
+			  <path class="st3" d="M114.5 183.1c5.6-1.8 12.9-4.2 20.7-6.7-1.8-5.6-4.2-12.9-6.7-20.7l-20.7 6.7 6.7 20.7z"/>
+			  <path class="st4" d="M158.1 168.9c7.8-2.5 15.1-4.9 20.7-6.7-1.8-5.6-4.2-12.9-6.7-20.7l-20.7 6.7 6.7 20.7z"/>
+			  <path class="st5" d="M175.5 115.1c5.7-1.9 8.8-8 7-13.7-1.9-5.7-8-8.8-13.7-7l-86.4 28.1c-5.3 1.9-8.3 7.7-6.7 13.2 1.7 5.8 7.8 9.3 13.4 7.4.2 0 86.4-28 86.4-28z"/>
+			  <path class="st6" d="M100.4 139.5c5.6-1.8 12.9-4.2 20.7-6.7-2.5-7.8-4.9-15.1-6.7-20.7l-20.7 6.7 6.7 20.7z"/>
+			  <path class="st7" d="M143.9 125.4c7.8-2.5 15.1-4.9 20.7-6.7-2.5-7.8-4.9-15.1-6.7-20.7l-20.7 6.7 6.7 20.7z"/>
+			</svg>
+			<span class="f4 ml1 mt1 pr2 b">Join us on Slack</span>
+		</a>
+	</article>
+@endsection
